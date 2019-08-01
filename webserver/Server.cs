6961398f7d42084535fea9562b7e70a7a39a2c93
vjola <vjola@tavisca.com>
2019-08-01 10:02:18 +0530

    Add project files.

---
+++ webserver/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webserver
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+              
+                    HttpListener web = new HttpListener();
+
+                    web.Prefixes.Add("http://localhost:8080/");
+                    Console.WriteLine("Listening..");
+                    web.Start();
+
+                    var context = web.GetContext();
+                    // Console.WriteLine(context);
+
+                    var response = context.Response;
+
+                    string filename = context.Request.RawUrl;
+                    filename = filename.Remove(0, 1);
+
+
+                    byte[] bytearray = StreamFile(filename);
+                    var buffer = bytearray;
+                    response.ContentLength64 = buffer.Length;
+                    var output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    //Console.WriteLine(output);
+                    //output.Close();
+
+                    web.Stop();
+                    //Console.ReadKey();
+              }
+            catch(Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+        }
+        public  static byte[] StreamFile(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            byte[] ImageData = System.IO.File.ReadAllBytes(filename);
+            fs.Read(ImageData, 0, System.Convert.ToInt32(fs.Length));
+            fs.Close();
+            return ImageData;
+        }
+
+    }
+}
+
+  
